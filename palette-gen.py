a7aece8852510764762530eb1e9fe3706548ad28
Nicholas Berridge-Argent <n.berridge-argent@student.unsw.edu.au>
2018-12-20 16:53:20 +1100

    Changed palette

--- palette-gen.py
+++ palette-gen.py
@@ -3,13 +3,10 @@
 import math
 
 def clamp(x):
-    if x == 512:
-        return 1.0
-    else:
-        return (1/math.pi)*(math.sin(math.pi*(x-512.0)/512.0)/((x-512.0)/512.0))
+    return math.sqrt(1-math.pow((x-512.0)/512.0, 2))
 
 for i in range(0, 1024):
-    r = int(clamp(i)*(((1024.0-i)/1024.0)*255.0))
-    g = int(clamp(i)*(255.0*math.exp(-math.pow((i-512.0)/512.0, 2))))
-    b = int(clamp(i)*((i/1024.0)*(-128.0*math.cos((i/128.0)*math.pi)+127.0)))
+    r = int(clamp(i)*(255.0*math.exp(-math.pow((i-128.0)/192.0, 2))))
+    g = int(clamp(i)*(255.0*math.exp(-math.pow((i-512.0)/192.0, 2))))
+    b = int(clamp(i)*(255.0*math.exp(-math.pow((i-896.0)/192.0, 2))))
     print(str(r) + ', ' + str(g) + ', ' + str(b))
